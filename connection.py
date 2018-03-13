53ab66be74fc2c29bd11da58c23106c501094075
ark1729 <arnavkm@gmail.com>
2018-03-12 20:19:22 -0700

    Header

--- connection.py
+++ connection.py
@@ -1,7 +1,8 @@
 import requests
 
 def connection(url):
-    r = requests.get(url)
+    user_agent='python-requests/4.8.2 (Compatible; Animus Silica; mailto: silicaanimus@gmail.com)'
+    r = requests.get(url, headers={'User-Agent': user_agent}, timeout=10)
     try:
         r.raise_for_status()
     except Exception as exc:
